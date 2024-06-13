530185f8d981f78bb1f5e041a2fdc6e9062fbfa0
shruti jain <hrm.shruti@gmail.com>
2024-06-13 16:30:37 +0200

    Apply operators

--- src/App.js
+++ src/App.js
@@ -1,31 +1,79 @@
 import { useState } from "react";
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState("0");
   const [firstNumber, setFirstNumber] = useState([]);
-  const [operator, setOperator] = useState('');
+  const [secondNumber, setSecondNumber] = useState([]);
+  const [operator, setOperator] = useState("");
 
-  const getNumber = () => {
+  const operatorIsNull = () => operator === "";
+
+  const getNumber = (digits) => {
     let number = 0;
-    for (let i = 0; i < firstNumber.length; i++) {
-      number += firstNumber[i] * Math.pow(10, i);
+    for (let i = 0; i < digits.length; i++) {
+      number += digits[i] * Math.pow(10, i);
     }
-    setResult(number);
+    return number;
   };
 
-  const pushToFirstNumber = (digit) => {
-    const digits = firstNumber;
-    digits.unshift(digit);
-    setFirstNumber(digits);
-    getNumber();
+  const clear = () => {
+    setFirstNumber([]);
+    setSecondNumber([]);
+    setOperator("");
+    setResult("0");
+  }
+
+  const registerDigits = (digit) => {
+    let digits;
+    if (operatorIsNull()) {
+      digits = firstNumber;
+      digits.unshift(digit);
+      setFirstNumber(digits);
+      const number = getNumber(firstNumber);
+      setResult(number);
+    } else {
+      digits = secondNumber;
+      digits.unshift(digit);
+      setSecondNumber(digits);
+      const number1 = getNumber(firstNumber);
+      const number2 = getNumber(secondNumber);
+      setResult(`${number1} ${operator} ${number2}`);
+    }
+  };
+
+  const registerOperator = (symbol) => {
+    setOperator(symbol);
+    setResult(result + " " + symbol);
+  };
+
+  const applyOperator = () => {
+    const number1 = getNumber(firstNumber);
+    const number2 = getNumber(secondNumber);
+    switch (operator) {
+      case "/":
+        setResult(number1 / number2);
+        break;
+      case "*":
+        setResult(number1 * number2);
+        break;
+      case "+":
+        setResult(number1 + number2);
+        break;
+      case "-":
+        setResult(number1 - number2);
+        break;
+      default:
+        break;
+    }
+    setFirstNumber([]);
+    setSecondNumber([]);
+    setOperator("");
   };
 
   const numbers = () => {
     const numbersList = [];
 
     for (let i = 9; i > 0; i--) {
-      numbersList.push(
-        <button onClick={() => pushToFirstNumber(i)}>{i}</button>
-      );
+      numbersList.push(<button onClick={() => registerDigits(i)}>{i}</button>);
     }
     return numbersList;
   };
@@ -38,19 +86,19 @@ function App() {
         </div>
         <div className="digits">
           {numbers()}
-          <button>0</button>
+          <button onClick={() => registerDigits(0)}>0</button>
           <button>.</button>
-          <button>=</button>
+          <button onClick={() => applyOperator()}>=</button>
         </div>
         <div className="operator">
-          <button onClick={() => setOperator('/')}>/</button>
-          <button onClick={() => setOperator('*')}>*</button>
-          <button onClick={() => setOperator('-')}>-</button>
-          <button onClick={() => setOperator('+')}>+</button>
+          <button onClick={() => registerOperator("/")}>/</button>
+          <button onClick={() => registerOperator("*")}>*</button>
+          <button onClick={() => registerOperator("-")}>-</button>
+          <button onClick={() => registerOperator("+")}>+</button>
           <button>%</button>
         </div>
         <div className="specialOperator">
-          <button>AC</button>
+          <button onClick={() => clear()}>AC</button>
           <button>( )</button>
           <button>Del</button>
         </div>
